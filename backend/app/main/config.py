a063d5cd2a28513d23ff054212d69d7b1e8a5305
Gopi <gopikrish3004@gmailcom>
2021-02-07 14:58:12 +0530

    UI styling completed with README altered for frontend & added env config file for backend

--- backend/app/main/config.py
+++ backend/app/main/config.py
@@ -4,11 +4,16 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = False
+    DB_USERNAME = os.environ.get("DB_USERNAME")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_SERVER = os.environ.get("DB_SERVER")
+    DB_NAME = os.environ.get("DB_NAME")
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = """mysql://{user_name}:{password}@{server}/{db_name}""".format(
+        user_name=Config.DB_USERNAME, password=Config.DB_PASSWORD, server=Config.DB_SERVER, db_name=Config.DB_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
